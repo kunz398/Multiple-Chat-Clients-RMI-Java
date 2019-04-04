1fb8237a76c3e97a152ce020fdf50470edfd3204
Kunal Singh <kunz.kunal@hotmail.com>
2019-04-05 02:24:32 +1200

    Add files via upload

---
+++ src/client/ClientRMIGUI.java
@@ -0,0 +1,386 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import javafx.scene.layout.Border;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+/**
+ *
+ * @author kunz
+ */
+public class ClientRMIGUI extends JFrame implements ActionListener {
+
+private static final long serialVersionUID = 1L;	
+private JPanel textPanel, inputPanel;
+private JTextField textField;
+private String name, message;
+private Font meiryoFont = new Font("Meiryo", Font.PLAIN, 14);
+private javax.swing.border.Border blankBorder = BorderFactory.createEmptyBorder(10,10,20,10);//top,r,b,l
+private ChatClient3 chatClient;
+    private JList<String> list;
+    private DefaultListModel<String> listModel;
+    
+    protected JTextArea textArea, userArea;
+    protected JFrame frame;
+    protected JButton privateMsgButton, startButton, sendButton;
+    protected JPanel clientPanel, userPanel;
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        jTextField1 = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Current Users");
+
+        jButton1.setText("Send PvtMsg");
+
+        jButton2.setText("Send");
+
+        jButton3.setText("Start");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jTextArea2.setText("Welcome Enter Your Name and press Start\n");
+        jScrollPane2.setViewportView(jTextArea2);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(46, 46, 46)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(52, 52, 52)
+                        .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 473, Short.MAX_VALUE)
+                    .addComponent(jTextField1))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton1)
+                        .addGap(35, 35, 35)
+                        .addComponent(jButton3))
+                    .addComponent(jScrollPane2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+ 	public static void main(String args[]){
+		//set the look and feel to 'Nimbus'
+		try{
+			for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
+				if("Nimbus".equals(info.getName())){
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		}
+		catch(Exception e){
+			}
+		new ClientRMIGUI();
+		}//end main
+	
+	public ClientRMIGUI(){
+			
+		frame = new JFrame("Client Chat Console");	
+	
+		
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		        
+		    	if(chatClient != null){
+			    	try {
+			        	sendMessage("Has Left The Chat");
+			        	chatClient.serverIF.leaveChat(name);
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}		        	
+		        }
+		        System.exit(0);  
+		    }   
+		});
+		//-----------------------------------------
+		//remove window buttons and border frame
+		
+	    //frame.setUndecorated(true);
+	    //frame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+	
+		Container c = getContentPane();
+		JPanel outerPanel = new JPanel(new BorderLayout());
+		
+		outerPanel.add(getInputPanel(), BorderLayout.CENTER);
+		outerPanel.add(getTextPanel(), BorderLayout.NORTH);
+		
+		c.setLayout(new BorderLayout());
+		c.add(outerPanel, BorderLayout.CENTER);
+		c.add(getUsersPanel(), BorderLayout.WEST);
+
+		frame.add(c);
+		frame.pack();
+		frame.setAlwaysOnTop(false);
+		frame.setLocation(150, 150);
+		textField.requestFocus();
+	
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+                frame.setBackground(Color.BLACK);
+	}
+		public JPanel getTextPanel(){
+		String welcome = "Welcome enter your name and press Start to begin\n";
+		textArea = new JTextArea(welcome, 14, 34);
+		textArea.setMargin(new Insets(10, 10, 10, 10));
+		textArea.setFont(meiryoFont);
+                
+                textArea.setBackground(Color.BLACK);
+                textArea.setForeground(Color.GREEN);
+                
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		textPanel = new JPanel();
+		textPanel.add(scrollPane);
+                textPanel.setBackground(Color.BLACK);
+                
+		textPanel.setFont(new Font("Meiryo", Font.PLAIN, 14));
+		return textPanel;
+	}
+                public JPanel getInputPanel(){
+		inputPanel = new JPanel(new GridLayout(1, 1, 5, 5));
+		inputPanel.setBorder(blankBorder);
+                inputPanel.setBackground(Color.BLACK);
+		textField = new JTextField();
+		textField.setFont(meiryoFont);
+		inputPanel.add(textField);
+		return inputPanel;
+	}
+                public JPanel getUsersPanel(){
+
+		userPanel = new JPanel(new BorderLayout());
+		String  userStr = " Current Users      ";
+                    
+		JLabel userLabel = new JLabel(userStr, JLabel.CENTER);
+		userPanel.add(userLabel, BorderLayout.NORTH);
+		userLabel.setFont(new Font("Meiryo", Font.PLAIN, 16));
+
+		String[] noClientsYet = {"No other users"};
+		setClientPanel(noClientsYet);
+
+		clientPanel.setFont(meiryoFont);
+		clientPanel.setBackground(Color.BLACK);
+                userPanel.add(makeButtonPanel(), BorderLayout.SOUTH);
+		userPanel.setBorder(blankBorder);
+                userPanel.setBackground(Color.BLACK);             
+                
+		return userPanel;
+	}
+  public void setClientPanel(String[] currClients) {
+    	clientPanel = new JPanel(new BorderLayout());
+        listModel = new DefaultListModel<String>();
+       
+        for(String s : currClients){
+        	listModel.addElement(s);
+        }
+        if(currClients.length > 1){
+        	privateMsgButton.setEnabled(true);
+        }
+
+        //Create the list and put it in a scroll pane.
+        list = new JList<String>(listModel);
+        list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        list.setVisibleRowCount(8);
+        list.setFont(meiryoFont);
+        
+        JScrollPane listScrollPane = new JScrollPane(list);
+
+        clientPanel.add(listScrollPane, BorderLayout.CENTER);
+        userPanel.add(clientPanel, BorderLayout.CENTER);
+    }
+	public JPanel makeButtonPanel() {
+		sendButton = new JButton("Send ");
+		sendButton.addActionListener(this);
+		sendButton.setEnabled(false);              
+                frame.getRootPane().setDefaultButton(sendButton);
+              
+        privateMsgButton = new JButton("Send Private Message");
+        privateMsgButton.addActionListener(this);
+        privateMsgButton.setEnabled(false);
+
+		startButton = new JButton("Start ");
+		startButton.addActionListener(this);
+                 
+               
+		JPanel buttonPanel = new JPanel(new GridLayout(4, 1));
+		buttonPanel.add(privateMsgButton);
+		buttonPanel.add(new JLabel(""));
+		buttonPanel.add(startButton);
+		buttonPanel.add(sendButton);
+                    buttonPanel.setBackground(Color.BLACK);
+		return buttonPanel;
+	}	public void actionPerformed(ActionEvent e){
+
+		try {
+			//get connected to chat service
+			if(e.getSource() == startButton){
+				name = textField.getText();
+				if(name.length() != 0){
+					frame.setTitle(name + "'s console ");
+					textField.setText("");
+                                        
+					textArea.append("username : " + name + " connecting to chat...\n");
+					getConnected(name);
+					if(!chatClient.connectionProblem){
+						startButton.setEnabled(false);
+						sendButton.setEnabled(true);
+						}
+				}
+				else{
+					JOptionPane.showMessageDialog(frame, "Enter your name to Start");
+				}
+			}
+                        //get text and clear textField
+			if(e.getSource() == sendButton){
+				message = textField.getText();
+				textField.setText("");
+				sendMessage(message);
+				System.out.println("Sending message : " + message);
+			}
+
+			//send a private message, to selected users
+			if(e.getSource() == privateMsgButton){
+				int[] privateList = list.getSelectedIndices();
+
+				for(int i=0; i<privateList.length; i++){
+					System.out.println("selected index :" + privateList[i]);
+				}
+				message = textField.getText();
+				textField.setText("");
+				sendPrivate(privateList);
+			}
+
+		}
+		catch (RemoteException remoteExc) {
+			remoteExc.printStackTrace();
+		}
+
+	}//end actionPerformed
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Send a message, to be relayed to all chatters
+	 * @param chatMessage
+	 * @throws RemoteException
+	 */
+	private void sendMessage(String chatMessage) throws RemoteException {
+		chatClient.serverIF.updateChat(name, chatMessage);
+	}
+
+	/**
+	 * Send a message, to be relayed, only to selected chatters
+	 * @param chatMessage
+	 * @throws RemoteException
+	 */
+	private void sendPrivate(int[] privateList) throws RemoteException {
+		String privateMessage = "[PM from " + name + "] :" + message + "\n";
+		chatClient.serverIF.sendPM(privateList, privateMessage);
+	}
+
+
+        private void getConnected(String userName) throws RemoteException{
+		//remove whitespace and non word characters to avoid malformed url
+		String cleanedUserName = userName.replaceAll("\\s+","_");
+		cleanedUserName = userName.replaceAll("\\W+","_");
+		try {
+			chatClient = new ChatClient3(this, cleanedUserName);
+			chatClient.startClient();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+        /*------------------------->>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<_--------------------------------- kUnal*/
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+
+  
+}
